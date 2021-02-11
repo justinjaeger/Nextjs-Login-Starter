6337d3013ec7b60ab0804005bf0fefd524749115
Justin Jaeger <jjustinjaeger@gmail.com>
2021-02-10 20:23:20 -0500

    made a change to babel.config

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,21 @@
 module.exports = {
   presets: [
-    ["@babel/preset-env",
-    {
-      targets: {
-        node: "current",
+    [
+      "next/babel",
+      {
+        "preset-env": {},
+        "transform-runtime": {},
+        "styled-jsx": {},
+        "class-properties": {}
+      }
+    ],
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current",
+        },
       },
-    }],
+    ],
   ],
 };
